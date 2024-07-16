b0192075cd10287e52ab21151d9e80e38e70be4c
elementas <gudelis.j@gmail.com>
2024-07-16 12:32:27 +0300

    removing 'helmet' package

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import helmet from 'helmet';
 import requestInformation from './middleware/request-information';
 import requestLogger from './middleware/request-logger';
 import errorHandlers from './middleware/error-handlers';
@@ -9,7 +8,6 @@ const app = express();
 
 app.use(requestInformation);
 app.use(requestLogger);
-app.use(helmet());
 app.use(express.json());
 app.use(router);
 app.use(...errorHandlers);
